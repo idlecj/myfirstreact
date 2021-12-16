de2756696c50a5c58a739ea9f345db89eb326e75
CJ <69058560+idlecj@users.noreply.github.com>
2021-12-16 18:58:34 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NavBar from './components/NavBar';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
@@ -6,16 +7,25 @@ import Greet from './components/Greet';
 function App() {
   return (
     <div className="App">
+      
+      
       <header className="App-header">
+      <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://i.pinimg.com/474x/79/d6/bb/79d6bb092fb230829d9275c7c5caafd4.jpg"} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          happy
         </p>
 		
 		<p> haha xd </p>
 		
 		<Greet/>
 		<Button/>
+    <div style={{ 
+      backgroundImage: `url("https://i.pinimg.com/474x/79/d6/bb/79d6bb092fb230829d9275c7c5caafd4.jpg")` 
+    }}>
+      Hello World
+    </div>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+const NavBar = () => {
+    return(
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">ICS2608</a>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            
+            </div>
+            )
+}
+
+export default NavBar;
